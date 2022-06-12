e8061307b9973caed3d05039cde7b4d83964a121
test <mohitbharti1998@gmail.com>
2022-06-12 23:57:47 +0530

    Initial commit

---
+++ src/test/java/io/springapplication/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springapplication.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
