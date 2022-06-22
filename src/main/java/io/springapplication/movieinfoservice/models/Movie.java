e08caa22df7b491b9b5942e9aa418e702844eab0
test <mohitbharti1998@gmail.com>
2022-06-22 18:49:25 +0530

    Update Commit

--- src/main/java/io/springapplication/movieinfoservice/models/Movie.java
+++ src/main/java/io/springapplication/movieinfoservice/models/Movie.java
@@ -1,29 +1,24 @@
 package io.springapplication.movieinfoservice.models;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.TokenBufferDeserializer;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 @Document(collection = "movieInfo")
 public class Movie {
 
 
     @Id
-    private ObjectId id;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+    private long id;
 
     private List<String> type;
 
@@ -35,43 +30,5 @@ public class Movie {
 
     private List<String> stars;
 
-    public List<String> getType() {
-        return type;
-    }
 
-    public void setType(List<String> type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<String> getDirector() {
-        return director;
-    }
-
-    public void setDirector(List<String> director) {
-        this.director = director;
-    }
-
-    public List<String> getWriter() {
-        return writer;
-    }
-
-    public void setWriter(List<String> writer) {
-        this.writer = writer;
-    }
-
-    public List<String> getStars() {
-        return stars;
-    }
-
-    public void setStars(List<String> stars) {
-        this.stars = stars;
-    }
 }
